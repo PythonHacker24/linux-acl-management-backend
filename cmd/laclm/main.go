4cc788642a699c7c15b820f2856e59ad95c47370
PythonHacker24 <adityapatil24680@gmail.com>
2025-05-12 15:50:43 +0530

    Updated main.go with user interrupt handling wrappers

--- cmd/laclm/main.go
+++ cmd/laclm/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -11,9 +14,39 @@ func main() {
 }
 
 func exec() error {
-	return nil
+
+	/*
+		exec() wraps run() protecting it with user interrupts  
+	*/
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-interrupt
+		cancel()
+	}()
+
+	return run(ctx)
 }
 
-func run() {
+func run(ctx context.Context) error {
 
+	/*
+		whatever written here will be protected by graceful shutdowns
+		all the functions called must be async here and ready for graceful shutdowns
+	*/
+
+	<-ctx.Done()
+
+	/* 
+		after this, exit signal is triggered
+		following code must be executed to shutdown graceful shutdown
+		call all the kill switches with context
+	*/
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PythonHacker24/linux-acl-management-backend
 
 go 1.24.2
+
+require github.com/bufbuild/buf v1.53.0 // indirect
