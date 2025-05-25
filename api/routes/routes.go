48b94a2fac7af932d46395688c39ea2643851bb8
PythonHacker24 <adityapatil24680@gmail.com>
2025-05-25 12:03:00 +0530

    Added route for listing files in a directory with authentication and logging middleware

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/PythonHacker24/linux-acl-management-backend/api/middleware"
 	"github.com/PythonHacker24/linux-acl-management-backend/internal/auth"
 	"github.com/PythonHacker24/linux-acl-management-backend/internal/health"
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/traversal"
 )
 
 /* all routes for all features are registered here */
@@ -19,5 +20,12 @@ func RegisterRoutes(mux *http.ServeMux) {
 	/* for monitoring the state of overall server and laclm backend */
 	mux.Handle("GET /health", http.HandlerFunc(
 		middleware.LoggingMiddleware(health.HealthHandler),
+	))
+
+	/* for listing files in a directory */
+	mux.Handle("POST /traverse/list-files", http.HandlerFunc(
+		middleware.LoggingMiddleware(
+			middleware.AuthenticationMiddleware(traversal.ListFilesInDirectory),
+		),
 	))
 }
