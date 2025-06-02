67155da62626e604ecb25ea60c5633ccac7d0f52
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-02 16:44:44 +0530

    Interchanged manager and interact files in sessions as naming conventions and added dedicated session redis for all sessions spawned

--- internal/session/interact.go
+++ internal/session/interact.go
@@ -2,29 +2,168 @@ package session
 
 import (
 	"container/list"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/PythonHacker24/linux-acl-management-backend/config"
 )
 
-/* create a new session manager */
-func NewManager() *Manager {
-	return &Manager{
-		sessionsMap:  make(map[string]*Session),
-		sessionOrder: list.New(),
-	}
-}
+/* for creating a session for user - used by HTTP HANDLERS */
+func (m *Manager) CreateSession(username, ipAddress, userAgent string) error {
 
-/* get next session for round robin */
-func (m *Manager) GetNextSession() *Session {
+	/* lock the ActiveSessions mutex till the function ends */
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	/* check if sessionOrder is empty */
-	if m.sessionOrder.Len() == 0 {
-		return nil 
+	/* check if session exists */
+	if _, exists := m.sessionsMap[username]; exists {
+		return fmt.Errorf("user already exists in active sessions")
 	}
 
-	element := m.sessionOrder.Front()
-	session := element.Value.(*Session)
-	
-	m.sessionOrder.MoveToBack(element)
-	return session 
+	/* Generate session metadata */
+	sessionID := uuid.New().String()
+	now := time.Now()
+
+	/* create the session */
+	session := &Session{
+		ID: sessionID,
+		Status: StatusActive,
+		Username: username,
+		IP: ipAddress,
+		UserAgent: userAgent,
+		Expiry:   time.Now().Add(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour),
+		CreatedAt:        now,
+		LastActiveAt:     now,
+		Timer: time.AfterFunc(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour,
+			func() { m.ExpireSession(username) },
+		),
+		CompletedCount:   0,
+		FailedCount:      0,
+		TransactionQueue:  list.New(),
+	}
+
+	/* add session to active sessions map and list */
+	element := 	m.sessionOrder.PushBack(session)
+	session.listElem = element
+
+	m.sessionsMap[username] = session
+
+	return nil
 }
+
+/* for expiring a session */
+func (m *Manager) ExpireSession(username string) {
+	/* thread safety for the manager */
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	/* check if user exists in active sessions */
+	session, ok := m.sessionsMap[username]
+	if !ok {
+		return
+	}
+
+	/* mark the session as expired */
+	session.Status = StatusExpired
+
+	/* remove session from sessionOrder Linked List */
+	if session.listElem != nil {
+		m.sessionOrder.Remove(session.listElem)
+	}
+
+	/* TODO: Add expired session to REDIS for persistent logging */
+
+	/* remove session from sessionsMap */
+	delete(m.sessionsMap, username)
+}
+
+/* add transaction to a session */
+func (m *Manager) AddTransaction(username string, txn interface{}) error {
+	/* thread safety for the manager */
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	/* get the session from sessions map with O(1) runtime */
+	session, exists := m.sessionsMap[username]
+	if !exists {
+		return fmt.Errorf("Session not found")
+	}
+
+	/* thread safety for the session */
+	session.Mutex.Lock()
+	defer session.Mutex.Unlock()
+
+	/* push transaction into the queue from back */
+	session.TransactionQueue.PushBack(txn)
+
+	return nil
+}
+
+/* refresh the session timer */
+func (m *Manager) refreshTimer(username string) error {
+	/* thread safety for the manager */
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	/* get session from sessionMap */
+	session, exists := m.sessionsMap[username]
+	if !exists {
+		return fmt.Errorf("Session not found")
+	}
+
+	/* thread safety for the session */
+	session.Mutex.Lock()
+	defer session.Mutex.Unlock()
+
+	/* reset the expiry time and last active time */
+	session.Expiry = time.Now().Add(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour)
+	session.LastActiveAt = time.Now()
+
+	/* stop the session timer */
+	if session.Timer != nil {
+		session.Timer.Stop()
+	}
+	
+	/* reset the session timer */
+	session.Timer = time.AfterFunc(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour,
+			func() { m.ExpireSession(username) },
+		) 
+
+	return nil
+}
+
+/* convert session information into frontend safe structure */
+func (m *Manager) toDashboardView(username string) (SessionView, error) {
+	/* thread safety for the manager */
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	/* get session from sessionMap */
+	session, exists := m.sessionsMap[username]
+	if !exists {
+		return SessionView{}, fmt.Errorf("Session not found")
+	}
+
+	/* thread safety for the session */
+	session.Mutex.Lock()
+	defer session.Mutex.Unlock()
+	
+	/* can be directly served as JSON in handler */
+	return SessionView{
+		ID:             session.ID,
+		Username:       session.Username,
+		IP:             session.IP,
+		UserAgent:      session.UserAgent,
+		CreatedAt:      session.CreatedAt,
+		LastActiveAt:   session.LastActiveAt,
+		Expiry:         session.Expiry,
+		CompletedCount: session.CompletedCount,
+		FailedCount:    session.FailedCount,
+		PendingCount:   session.TransactionQueue.Len(),
+	}, nil
+} 
+
--- internal/session/manager.go
+++ internal/session/manager.go
@@ -2,13 +2,9 @@ package session
 
 import (
 	"container/list"
-	"fmt"
 	"sync"
-	"time"
 
-	"github.com/google/uuid"
-
-	"github.com/PythonHacker24/linux-acl-management-backend/config"
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/session/redis"
 )
 
 /*
@@ -23,159 +19,31 @@ type Manager struct {
 	sessionsMap		map[string]*Session
 	sessionOrder	*list.List
 	mutex 			sync.RWMutex
+	redis 			redis.RedisClient
 }
 
-/* for creating a session for user - used by HTTP HANDLERS */
-func (m *Manager) CreateSession(username, ipAddress, userAgent string) error {
+/* create a new session manager */
+func NewManager(redis redis.RedisClient) *Manager {
+	return &Manager{
+		sessionsMap:  make(map[string]*Session),
+		sessionOrder: list.New(),
+		redis:	redis,
+	}
+}
 
-	/* lock the ActiveSessions mutex till the function ends */
+/* get next session for round robin */
+func (m *Manager) GetNextSession() *Session {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	/* check if session exists */
-	if _, exists := m.sessionsMap[username]; exists {
-		return fmt.Errorf("user already exists in active sessions")
+	/* check if sessionOrder is empty */
+	if m.sessionOrder.Len() == 0 {
+		return nil 
 	}
 
-	/* Generate session metadata */
-	sessionID := uuid.New().String()
-	now := time.Now()
-
-	/* create the session */
-	session := &Session{
-		ID: sessionID,
-		Status: StatusActive,
-		Username: username,
-		IP: ipAddress,
-		UserAgent: userAgent,
-		Expiry:   time.Now().Add(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour),
-		CreatedAt:        now,
-		LastActiveAt:     now,
-		Timer: time.AfterFunc(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour,
-			func() { m.ExpireSession(username) },
-		),
-		CompletedCount:   0,
-		FailedCount:      0,
-		TransactionQueue:  list.New(),
-	}
-
-	/* add session to active sessions map and list */
-	element := 	m.sessionOrder.PushBack(session)
-	session.listElem = element
-
-	m.sessionsMap[username] = session
-
-	return nil
-}
-
-/* for expiring a session */
-func (m *Manager) ExpireSession(username string) {
-	/* thread safety for the manager */
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	/* check if user exists in active sessions */
-	session, ok := m.sessionsMap[username]
-	if !ok {
-		return
-	}
-
-	/* disable the session */
-	session.Status = StatusExpired
-
-	/* remove session from sessionOrder Linked List */
-	if session.listElem != nil {
-		m.sessionOrder.Remove(session.listElem)
-	}
-
-	/* TODO: Add expired session to REDIS for persistent logging */
-
-	/* remove session from sessionsMap */
-	delete(m.sessionsMap, username)
-}
-
-/* add transaction to a session */
-func (m *Manager) AddTransaction(username string, txn interface{}) error {
-	/* thread safety for the manager */
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	/* get the session from sessions map with O(1) runtime */
-	session, exists := m.sessionsMap[username]
-	if !exists {
-		return fmt.Errorf("Session not found")
-	}
-
-	/* thread safety for the session */
-	session.Mutex.Lock()
-	defer session.Mutex.Unlock()
-
-	/* push transaction into the queue from back */
-	session.TransactionQueue.PushBack(txn)
-
-	return nil
-}
-
-/* refresh the session timer */
-func (m *Manager) RefreshTimer(username string) error {
-	/* thread safety for the manager */
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	/* get session from sessionMap */
-	session, exists := m.sessionsMap[username]
-	if !exists {
-		return fmt.Errorf("Session not found")
-	}
-
-	/* thread safety for the session */
-	session.Mutex.Lock()
-	defer session.Mutex.Unlock()
-
-	/* reset the expiry time and last active time */
-	session.Expiry = time.Now().Add(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour)
-	session.LastActiveAt = time.Now()
-
-	/* stop the session timer */
-	if session.Timer != nil {
-		session.Timer.Stop()
-	}
+	element := m.sessionOrder.Front()
+	session := element.Value.(*Session)
 	
-	/* reset the session timer */
-	session.Timer = time.AfterFunc(time.Duration(config.BackendConfig.AppInfo.SessionTimeout) * time.Hour,
-			func() { m.ExpireSession(username) },
-		) 
-
-	return nil
+	m.sessionOrder.MoveToBack(element)
+	return session 
 }
-
-/* convert session information into frontend safe structure */
-func (m *Manager) ToDashboardView(username string) (SessionView, error) {
-	/* thread safety for the manager */
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-
-	/* get session from sessionMap */
-	session, exists := m.sessionsMap[username]
-	if !exists {
-		return SessionView{}, fmt.Errorf("Session not found")
-	}
-
-	/* thread safety for the session */
-	session.Mutex.Lock()
-	defer session.Mutex.Unlock()
-	
-	/* can be directly served as JSON in handler */
-	return SessionView{
-		ID:             session.ID,
-		Username:       session.Username,
-		IP:             session.IP,
-		UserAgent:      session.UserAgent,
-		CreatedAt:      session.CreatedAt,
-		LastActiveAt:   session.LastActiveAt,
-		Expiry:         session.Expiry,
-		CompletedCount: session.CompletedCount,
-		FailedCount:    session.FailedCount,
-		PendingCount:   session.TransactionQueue.Len(),
-	}, nil
-} 
