342b9b3b2d43bed4d7d3f6b25dadccea3e4e107a
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-02 22:19:49 +0530

    Updated transprocessor for using shared types

--- internal/transprocessor/perm-processor.go
+++ internal/transprocessor/perm-processor.go
@@ -4,18 +4,20 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/PythonHacker24/linux-acl-management-backend/internal/session"
 	"go.uber.org/zap"
+
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/session"
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/types"
 )
 
 /* instanciate new permission processor */
 func NewPermProcessor() *PermProcessor {
-	return &PermProcessor{}	
+	return &PermProcessor{}
 }
 
 /* processor for permissions manager */
 func (p *PermProcessor) Process(ctx context.Context, curSession *session.Session, tx interface{}) error {
-	transaction, ok := tx.(*Transaction) 
+	transaction, ok := tx.(*types.Transaction)
 	if !ok {
 		return fmt.Errorf("invalid transaction type")
 	}
@@ -24,25 +26,25 @@ func (p *PermProcessor) Process(ctx context.Context, curSession *session.Session
 	zap.L().Info("Processing Transaction",
 		zap.String("user", curSession.Username),
 	)
-	
+
 	select {
-		case <-ctx.Done():
-			/* 
-				store this into persistent storage too!
-				make sure database connections are closed after scheduler shutsdown
-			*/
-			zap.L().Warn("Transaction process stopped due to shutdown",
-				zap.String("user", curSession.Username),	
-			)
-			return ctx.Err()
-		default:
-			/*
-				permprocessor hands over transactions to remoteprocessor/localprocessor depending upon request
-				remoteprocessor -> handles permissions on remote servers
-				localprocessor -> handles permissions on local system (where this backend is deployed)
-			*/
-			_ = transaction		
+	case <-ctx.Done():
+		/*
+			store this into persistent storage too!
+			make sure database connections are closed after scheduler shutsdown
+		*/
+		zap.L().Warn("Transaction process stopped due to shutdown",
+			zap.String("user", curSession.Username),
+		)
+		return ctx.Err()
+	default:
+		/*
+			permprocessor hands over transactions to remoteprocessor/localprocessor depending upon request
+			remoteprocessor -> handles permissions on remote servers
+			localprocessor -> handles permissions on local system (where this backend is deployed)
+		*/
+		_ = transaction
 	}
 
-	return nil	
+	return nil
 }
