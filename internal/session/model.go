696e541e21856744be62f153ea395df2d43b4223
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-03 23:29:48 +0530

    Added structure for PostgreSQL database archival

--- internal/session/model.go
+++ internal/session/model.go
@@ -74,3 +74,28 @@ type SessionView struct {
 	LastActiveAt    time.Time `json:"lastActiveAt"`
 	Expiry          time.Time `json:"expiry"`
 }
+
+/* session information structure in archival database */
+type ArchivedSession struct {
+	ID             string    `db:"id"`
+	Username       string    `db:"username"`
+	IP             string    `db:"ip"`
+	UserAgent      string    `db:"user_agent"`
+	Status         string    `db:"status"`
+	CreatedAt      time.Time `db:"created_at"`
+	LastActiveAt   time.Time `db:"last_active_at"`
+	Expiry         time.Time `db:"expiry"`
+	CompletedCount int       `db:"completed_count"`
+	FailedCount    int       `db:"failed_count"`
+	ArchivedAt     time.Time `db:"archived_at"`
+}
+
+/* NOTE: Still in development */
+/* transaction information structure in archival database */
+type ArchivedTransaction struct {
+	ID        string    `db:"id"`
+	SessionID string    `db:"session_id"`
+	Status    string    `db:"status"`
+	Output    string    `db:"output"`
+	CreatedAt time.Time `db:"created_at"`
+}
