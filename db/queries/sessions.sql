7a4c24339b58728ac69a3d318784af43d26eeac9
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-05 00:26:21 +0530

    updated names for functions in session.sql for better readibility

--- db/queries/sessions.sql
+++ db/queries/sessions.sql
@@ -1,4 +1,4 @@
--- name: CreateSession :one
+-- name: StoreSessionPQ :one
 INSERT INTO sessions_archive (
     id, username, ip, user_agent, status, 
     created_at, last_active_at, expiry
@@ -6,14 +6,14 @@ INSERT INTO sessions_archive (
     $1, $2, $3, $4, $5, $6, $7, $8
 ) RETURNING *;
 
--- name: GetSession :one
+-- name: GetSessionPQ :one
 SELECT * FROM sessions_archive 
 WHERE id = $1;
 
--- name: GetSessionByUsername :many
+-- name: GetSessionByUsernamePQ :many
 SELECT * FROM sessions_archive 
 WHERE username = $1
 ORDER BY created_at DESC;
 
--- name: DeleteSession :exec
+-- name: DeleteSessionPQ :exec
 DELETE FROM sessions_archive WHERE id = $1;
