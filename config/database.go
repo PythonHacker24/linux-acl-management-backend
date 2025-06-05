c04459ba4111ab81bc66825f8c105e0ebbba33fc
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-06 00:46:33 +0530

    Completed complete PostgreSQL user configuration for config.yaml with validation function

--- config/database.go
+++ config/database.go
@@ -10,6 +10,7 @@ import (
 /* database parameters */
 type Database struct {
 	TransactionLogRedis TransactionLogRedis `yaml:"transaction_log_redis,omitempty"`
+	ArchivalPQ			ArchivalPQ			`yaml:"archival_postgres,omitempty"`
 }
 
 /* transaction log redis parameters */
@@ -19,9 +20,31 @@ type TransactionLogRedis struct {
 	DB       int    `yaml:"db,omitempty"`
 }
 
-/* normalization function */
+/* archival PostgreSQL parameters */
+type ArchivalPQ struct {
+	Host		string 	`yaml:"host,omitempty"` 
+	Port		int		`yaml:"port,omitempty"`
+	User		string 	`yaml:"user,omitempty"`
+	Password	string 	`yaml:"password,omitempty"`
+	DBName		string 	`yaml:"dbname,omitempty"`
+	SSLMode		string 	`yaml:"sslmode,omitempty"`
+}
+
+/* normalization function for database */
 func (d *Database) Normalize() error {
-	return d.TransactionLogRedis.Normalize()
+	/* check if Redis parameters are valid */
+	err := d.TransactionLogRedis.Normalize()
+	if err != nil {
+		return err
+	}
+
+	/* check if PostgreSQL parameters are valid */
+	err = d.ArchivalPQ.Normalize()
+	if err != nil {
+		return err 
+	}
+
+	return nil
 }
 
 /* transaction log redis normalization function */
@@ -43,4 +66,40 @@ func (r *TransactionLogRedis) Normalize() error {
 	/* r.DB default value can be 0 */
 
 	return nil
+}
+
+/* archival PostgreSQL parameters */
+func (a *ArchivalPQ) Normalize() error {
+
+	/* return localhost if empty */
+	if a.Host == "" {
+        a.Host = "localhost"
+    }
+
+	/* return default port if empty */
+    if a.Port == 0 {
+        a.Port = 5432
+    }
+
+	/* username is mandatory */
+    if a.User == "" {
+        return errors.New("Database username is not set in the configuration.")
+    }
+
+	/* dbname is mandatory */
+    if a.DBName == "" {
+        return errors.New("Database name (dbname) is not set in the configuration.")
+    }
+
+	/* sslmode is disabled by default */
+    if a.SSLMode == "" {
+        a.SSLMode = "disable"
+    }
+
+	/* empty password but give a warning */
+    if a.Password == "" {
+        fmt.Printf("Warning: Connecting to PostgreSQL without a password. Consider using one for security.\n\n")
+    }
+
+    return nil
 }
