c15237d4c1c7e451d34dfde9d546da4abeadc5bf
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-08 01:18:49 +0530

    Fixed auth by creating seperate token module (dependency cycle)

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,84 +1,12 @@
 package auth
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
-	"time"
 
-	"github.com/PythonHacker24/linux-acl-management-backend/config"
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/token"
 )
 
-/* generating jwt token for user identification with specified configs */
-func GenerateJWT(username string) (string, error) {
-	expiryHours := config.BackendConfig.BackendSecurity.JWTExpiry
-
-	/* generate JWT token with claims */
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": username,
-		"exp":      time.Now().Add(time.Hour * time.Duration(expiryHours)).Unix(),
-	})
-
-	return token.SignedString([]byte(config.BackendConfig.BackendSecurity.JWTTokenSecret))
-}
-
-/* validate JWT token and return claims */
-func ValidateJWT(tokenString string) (jwt.MapClaims, error) {
-
-	/* parse the token */
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method")
-		}
-		return []byte(config.BackendConfig.BackendSecurity.JWTTokenSecret), nil
-	})
-
-	/* check if token is valid */
-	if err != nil {
-		return nil, fmt.Errorf("JWT parsing error: %w", err)
-	}
-
-	/* check if token is valid */
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims, nil
-	}
-
-	return nil, fmt.Errorf("invalid token")
-}
-
-/* extracts username from JWT token */
-func GetUsernameFromJWT(tokenString string) (string, error) {
-
-	/* get claims from JWT Token */
-	claims, err := ValidateJWT(tokenString)
-	if err != nil {
-		return "", fmt.Errorf("JWT validation error: %w", err)
-	}
-
-	/* extract username from JWT Token */
-	username, ok := claims["username"].(string)
-	if !ok {
-		return "", fmt.Errorf("username not found in token")
-	}
-
-	return username, nil
-}
-
-/* extract username from http request (wrapper around GetUsernameFromJWT for http requests) */
+/* extract username from http request */
 func ExtractUsernameFromRequest(r *http.Request) (string, error) {
-
-	/* extract authentication hearder from http request */
-	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		return "", fmt.Errorf("missing Authorization header")
-	}
-
-	/* parse the token from the header */
-	tokenParts := strings.Split(authHeader, " ")
-	if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
-		return "", fmt.Errorf("invalid Authorization header format")
-	}
-
-	return GetUsernameFromJWT(tokenParts[1])
+	return token.ExtractUsernameFromRequest(r)
 }
