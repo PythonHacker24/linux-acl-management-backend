b67c0b9fffc7e846570d032204d1524ce4051065
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-10 01:11:05 +0530

    Updated error channel

--- internal/transprocessor/model.go
+++ internal/transprocessor/model.go
@@ -4,11 +4,7 @@ package transprocessor
 	transprocessor implements the transactions structure that whole project complies with
 */
 
-/* universal transaction structure */
-type Transaction struct {
-	ID      string
-	/* command structure would be added later */
-}
-
 /* permissions processor */
-type PermProcessor struct{}
+type PermProcessor struct {
+	errCh		chan<-error
+}
