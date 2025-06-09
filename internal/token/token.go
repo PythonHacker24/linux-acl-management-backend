dee18d7ce2291f4419e3080cf48bf1bf4ae9c05f
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-10 01:10:58 +0530

    Adding session ID (UUID as string) parameter in JWT token

--- internal/token/token.go
+++ internal/token/token.go
@@ -7,17 +7,19 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 
 	"github.com/PythonHacker24/linux-acl-management-backend/config"
 )
 
 /* generating jwt token for user identification with specified configs */
-func GenerateJWT(username string) (string, error) {
+func GenerateJWT(username string, sessionID uuid.UUID) (string, error) {
 	expiryHours := config.BackendConfig.BackendSecurity.JWTExpiry
 
 	/* generate JWT token with claims */
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"username": username,
+		"sessionID": sessionID, 
 		"exp":      time.Now().Add(time.Hour * time.Duration(expiryHours)).Unix(),
 	})
 
@@ -47,37 +49,42 @@ func ValidateJWT(tokenString string) (jwt.MapClaims, error) {
 	return nil, fmt.Errorf("invalid token")
 }
 
-/* extracts username from JWT token */
-func GetUsernameFromJWT(tokenString string) (string, error) {
+/* extracts username and sessionID from JWT token */
+func GetDataFromJWT(tokenString string) (string, string, error) {
 	/* get claims from JWT Token */
 	claims, err := ValidateJWT(tokenString)
 	if err != nil {
-		return "", fmt.Errorf("JWT validation error: %w", err)
+		return "", "", fmt.Errorf("JWT validation error: %w", err)
 	}
 
 	/* extract username from JWT Token */
 	username, ok := claims["username"].(string)
 	if !ok {
-		return "", fmt.Errorf("username not found in token")
+		return "", "", fmt.Errorf("username not found in token")
 	}
 
-	return username, nil
+	/* extract sessionID from JWT Token */
+	sessionID, ok := claims["sessionID"].(string)
+	if !ok {
+		return "", "", fmt.Errorf("sessionID not found in token")
+	}
+	return username, sessionID, nil
 }
 
-/* extract username from http request (wrapper around GetUsernameFromJWT for http requests) */
-func ExtractUsernameFromRequest(r *http.Request) (string, error) {
+/* extract username and sessionID from http request (wrapper around GetUsernameFromJWT for http requests) */
+func ExtractDataFromRequest(r *http.Request) (string, string, error) {
 	/* get the authorization header */
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		return "", fmt.Errorf("authorization header not found")
+		return "", "", fmt.Errorf("authorization header not found")
 	}
 
 	/* check if the header is in the correct format */
 	parts := strings.Split(authHeader, " ")
 	if len(parts) != 2 || parts[0] != "Bearer" {
-		return "", fmt.Errorf("invalid authorization header format")
+		return "","",  fmt.Errorf("invalid authorization header format")
 	}
 
-	/* extract username from JWT token */
-	return GetUsernameFromJWT(parts[1])
+	/* extract username and sessionID from JWT token */
+	return GetDataFromJWT(parts[1])
 }
