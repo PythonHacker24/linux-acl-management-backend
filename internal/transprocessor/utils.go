44b47862d1b5cc7675e7af6090ba8e11cc6c0e17
PythonHacker24 <adityapatil24680@gmail.com>
2025-07-03 00:20:57 +0530

    Find file server with path

---
+++ internal/transprocessor/utils.go
@@ -0,0 +1,26 @@
+package transprocessor 
+
+import (
+	"strings"
+
+	"github.com/PythonHacker24/linux-acl-management-backend/config"
+)
+
+func FindServerFromPath(servers []config.FileSystemServers, filepath string) (isRemote bool, host string, port int, found bool, absolutePath string) {
+	/* search through all the servers */
+	for _, server := range servers {
+		/* check if the server path has the prefix for filepath */
+		if strings.HasPrefix(filepath, server.Path) {
+			absolutePath := strings.TrimPrefix(filepath, server.Path)
+			/* check if it's remote */
+			if server.Remote != nil {
+				return true, server.Remote.Host, server.Remote.Port, true, absolutePath
+			}
+			/* local filesystem */
+			return false, "", 0, true, absolutePath
+		}
+	}
+
+	/* filesystem not found */
+	return false, "", 0, false, ""
+}
