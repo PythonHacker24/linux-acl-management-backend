d0abc42525241730db4534dbeaf1ab333207bf02
PythonHacker24 <adityapatil24680@gmail.com>
2025-07-03 00:21:05 +0530

    Added remote transaction execution

---
+++ internal/transprocessor/remotetxn.go
@@ -0,0 +1,72 @@
+package transprocessor
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/PythonHacker24/linux-acl-management-backend/internal/types"
+	protos "github.com/PythonHacker24/linux-acl-management-backend/proto"
+	"go.uber.org/zap"
+)
+
+/* takes a transactions and attempts to execute it via daemons */
+func (p *PermProcessor) HandleRemoteTransaction(host string, port int, txn *types.Transaction, absolutePath string) error {
+
+	/* if gRPCPool is nil, return an error */
+	if p.gRPCPool == nil { 
+		return fmt.Errorf("gRPC pool is nil") 
+	}
+
+	/* get connection to the respective daemon */
+	address := fmt.Sprintf("%s:%d", host, port) 
+	conn, err := p.gRPCPool.GetConn(address, p.errCh)
+	if err != nil {
+		zap.L().Error("Failed to get connect with a daemon", 
+			zap.String("Address", address), 
+			zap.Error(err),
+		)
+	}
+
+	/* make it a for loop for interating all entries */
+	aclpayload := &protos.ACLEntry{
+		EntityType: txn.Entries.EntityType,
+		Entity: txn.Entries.Entity,
+		Permissions: txn.Entries.Permissions,
+		Action: txn.Entries.Action,
+		IsDefault: txn.Entries.IsDefault,
+	}
+
+	/* build the request for daemon */
+	request := &protos.ApplyACLRequest{
+		TransactionID: txn.ID.String(),
+		TargetPath: absolutePath,
+		Entry: aclpayload,
+	}
+
+	/* MAKE IT CONFIGURABLE */
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Minute)	
+
+	aclClient := protos.NewACLServiceClient(conn)
+	aclResponse, err := aclClient.ApplyACLEntry(ctx, request)
+	if err != nil {
+		p.errCh <- fmt.Errorf("failed to send ACL request to daemon")
+	}
+
+	if aclResponse.Success {
+		/* set transaction successful*/
+
+		/* REMOVE THIS */
+		zap.L().Info("Transaction seems successful", 
+			zap.String("transaction ID", txn.ID.String()),
+		)
+
+		txn.Output = "ACL executed successfully on filesystem servers"
+		
+		txn.ExecStatus = true
+	} else {
+		txn.ErrorMsg = "ACL failed to get executed in the filesystem server"
+	}
+
+	return nil
+}
