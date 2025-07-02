f23a4fb14f59d4bf2d901de31e9c5e0151bc5527
PythonHacker24 <adityapatil24680@gmail.com>
2025-07-03 00:22:12 +0530

    Added place for gRPC pool to handle all over the processor

--- internal/transprocessor/model.go
+++ internal/transprocessor/model.go
@@ -1,10 +1,13 @@
 package transprocessor
 
+import "github.com/PythonHacker24/linux-acl-management-backend/internal/grpcpool"
+
 /*
 	transprocessor implements the transactions structure that whole project complies with
 */
 
 /* permissions processor */
 type PermProcessor struct {
+	gRPCPool    *grpcpool.ClientPool
 	errCh		chan<-error
 }
