672930073a0b806058f1a0f850615c0aeee66df6
PythonHacker24 <adityapatil24680@gmail.com>
2025-07-03 00:19:53 +0530

    Updated test.sh script

--- test.sh
+++ test.sh
@@ -3,7 +3,11 @@
 # Step 1: Login to get the token
 login_response=$(curl -s -X POST http://localhost:8080/login \
   -H "Content-Type: application/json" \
-  -d '{"username": "Jessica Pearson", "password": "iamthebest"}')
+  -d '{"username": "Gregory House", "password": "idontlikepeople"}')
+
+echo "$login_response"
+
+sleep 1
 
 # Step 2: Extract token using jq (requires jq to be installed)
 token=$(echo "$login_response" | jq -r '.token')
@@ -24,14 +28,12 @@ while true; do
       -H "Content-Type: application/json" \
       -d '{
         "operation": "setfacl",
-        "targetPath": "/var/www/html",
-        "entries": [
-          {
+        "targetPath": "/mnt/beegfs-1/home/maverick/linux-acl-management-aclapi/README.md",
+        "entries": {
             "entityType": "user",
-            "entity": "alice",
-            "permissions": "rwx",
-            "action": "add"
+            "entity": "tommy",
+            "permissions": "rw-",
+            "action": "modify"
           }
-        ]
       }'
 done
